d5d990cc66819b106a4787b194a20ca06a2253cc
João Bradachi <joao.bradachi@ufv.br>
2023-10-11 10:25:32 -0300

    ideia para armazenar os valores dos passos

--- TadBT.c
+++ TadBT.c
@@ -1,8 +1,9 @@
 #include "./headers/TadBT.h"
 
-int IniciaBT(Mapa* mapa){
-    int v[4] = {1, 1, 0, 0}, h[4] = {0, 0, 1, 1}, q = 0;
-    RecursaoBT(&mapa, 0, 0, q, v, h, 1, 0);
+int IniciaBT(TMapa* mapa){
+    int v[4] = {0, -1, 0, 1}, h[4] = {1, 0, -1, 0}, q = 0; // com essa configutação indiana bones anda -> direita -> baixo -> esquerda -> cima.
+    mapa->mapa[0][0].passo = 1; // ponto de início
+    RecursaoBT(mapa, 0, 0, q, v, h, 1, 0);
     if (q){
         // retorna resultado obtido
     }else{
@@ -10,31 +11,46 @@ int IniciaBT(Mapa* mapa){
     }
 }
 
-int RecursaoBT(Mapa* mapa, int posX, int posY, int q, int v[4], int h[4], int passo, int chavesObtidas){
-    int xn, yn, q1, i=-1;
+int RecursaoBT(TMapa* mapa, int posX, int posY, int q, int v[4], int h[4], int passo, int chavesObtidas){
+    int xn, yn, q1, i=-1; // só pra i começar em 0 no while
     
     while(q1 || i<3){
         i++;
         q1 = 0;
         xn = posX + h[i];
-        yn = posY + h[i];
-        if(xn < mapa->coluna && yn < mapa->linha && mapa->mapa[xn][yn] != '1' /* && mapa->mapa[xn][yn] != de caminho já marcado*/){
-            mapa->mapa[posX][posY] = "0"; // marcar caminho com passo >>> verificar amanhã
-            if(mapa->mapa[xn][yn] == 'C'){
-                chavesObtidas++;
-            }
-            else if((chavesObtidas == mapa->qntChaves) && (mapa->mapa[xn][yn] == 'X')){
-                // chegou no baú
-            }
-            // caso de não haver solução >>> leitura pode guardar quantas casas é possível andar no máximo
-            // if (passo < quantPassosTotal){
-            RecursaoBT(&mapa, xn, yn, q1, v, h, passo+1, chavesObtidas);
-            if(q1 == 0){
-                mapa->mapa[posX][posY] = "0"; // volta o passo, desmarca o caminho
-            }
-            else{
-                q1 = 1;
-            }
+        yn = posY + v[i];
+        if((xn < mapa->coluna) && (yn < mapa->linha) && (mapa->mapa[xn][yn].tipo != '1') && (mapa->mapa[xn][yn].passo == 0)){ // != "1" testa se é diferente de parede
+            // caso de não houver solução >>> andar 
+            //if (passo < quantPassosTotal){
+                if((chavesObtidas != mapa->qntChaves) && (mapa->mapa[xn][yn].tipo == 'X')){
+                    // chegou no baú sem chave, volta pra buscar as chaves 
+                    q1 = 0; // analizar pra q serve q1
+                    return 0;
+                }
+                else if((chavesObtidas == mapa->qntChaves) && (mapa->mapa[xn][yn].tipo == 'X')){
+                    // chegou no baú
+                    q1 = 1; // analizar pra q serve q1
+                    // salva a matriz
+                }
+
+                mapa->mapa[xn][yn].passo = passo+1; // ultima passada no baú conta como passo?
+                
+                if(mapa->mapa[xn][yn].tipo == 'C'){
+                    chavesObtidas++;
+                }
+                
+                
+
+                RecursaoBT(mapa, xn, yn, q1, v, h, passo+1, chavesObtidas);
+                if(q1 == 0){
+                    mapa->mapa[xn][yn].passo = 0; // volta o passo, desmarca o caminho
+                    if(mapa->mapa[xn][yn].tipo == 'C'){
+                        chavesObtidas--;
+                    }
+                }
+                else{
+                    q1 = 1;
+                }
             //}
             
         }    
--- headers/TadMapa.h
+++ headers/TadMapa.h
@@ -2,15 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Celula{
+    char tipo; // tipo de cada celula da matriz
+    int passo; // passo do indiana bones, se 0 então ele não passou por lá
+}TCelula;
+
+// Como salvar a matriz? a matriz resposta faz parte do tad ou é uma matriz separada?
+// para o caso de mostrar todas as possibilidades de caminhos colocar flag na recursãoBT e dentro dela chamar o mostra matriz
+// é interesante criar um tad resposta pra ir guardando a matriz e se possúi resposta, porque vai acomulando respostas certas e queremos encontrar amelhor solução
+// a flag q ainda continua sendo útil para percorrer a matriz e ir voltando os passos
+// o fim de RecursaoBT sempre será 0 pois BT tentará fazer todos os caminhos possíveis
+
 typedef struct Mapa{
-    char** mapa;
+    TCelula** mapa;
     int linha;
     int coluna;
     int qntChaves;
     int locBau[2];
-}Mapa;
+    int qntParedes;
+}TMapa;
 
-int alocaMatriz(Mapa *mapa);
-int mostraMatriz(Mapa *mapa);
-int preencheMatrizTeste(Mapa *mapa);
-int manipulaArquivo(char *pTexto, Mapa *mapa);
+int alocaMatriz(TMapa *mapa);
+int mostraMatriz(TMapa *mapa);
+int preencheMatrizTeste(TMapa *mapa);
+int manipulaArquivo(char *pTexto, TMapa *mapa);
