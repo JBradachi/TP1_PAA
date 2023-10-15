9a53e650fa7797c4f1a3690c8aaae174ef32e5dd
João Bradachi <joao.bradachi@ufv.br>
2023-10-15 14:01:14 -0300

    Update TadBT.h

--- headers/TadBT.h
+++ headers/TadBT.h
@@ -2,7 +2,6 @@
 
 int IniciaBT(TMapa* mapa, TMapa* resultado);
 
-
 int RecursaoBT(TMapa* mapa, TMapa* resultado, int posX, int posY, int v[4], int h[4], int passo, int chavesObtidas);
 
 int salvaResultado(TMapa* mapa, TMapa* resultado);
