6d82d38849ddaf143e98ef54a2e24bf6e6b38378
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2023-10-17 13:32:03 -0300

    criado files novos

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 int main(){
     TMapa m1, resultado;
     char path[100] = "./entradas/";
-    char arquivo[30] = "teste.txt";
+    char arquivo[30] = "teste copy.txt";
+    printf("Digite o nome do arquivo: ");
+    scanf("%s", &arquivo);
 
     strcat(path, arquivo);
     manipulaArquivo(path, &m1);
