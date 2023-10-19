27d0bbd95c0547991f92219ffa7d3c60960a8b27
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2023-10-18 22:57:07 -0300

    mostraVetorGlobo sem garantia de certeza

--- sources/TadBT.c
+++ sources/TadBT.c
@@ -210,7 +210,7 @@ int IniciaGloboBT(TMapa* mapa, TMapa* resultado, char mostrarPossibilidades, cha
         printf("\nMatriz resultado: \n");
         mostraResultadoGrafico(resultado);
         printf("\nVetor resultado: \n");
-        //mostraVetorResultadoGlobo(resultado, 0, 0, 1);
+        mostraVetorResultadoGlobo(resultado, 0, 0, 1);
         printf("\nForam encontrados %d caminho(s) diferente(s)\n", quantCaminhos);
 
     }
@@ -263,8 +263,8 @@ int RecursaoGloboBT(TMapa* mapa, TMapa* resultado, int posX, int posY, int movim
             // se for chave aumenta a quantidade de chaves obtidas
             if(mapa->mapa[xn][yn].tipo == Chave){
                 chavesObtidas++;
-                printf("Total de chaves: %d\n", chavesObtidas);
-                mostraResultadoGrafico(mapa);
+                //printf("Total de chaves: %d\n", chavesObtidas);
+                //mostraResultadoGrafico(mapa);
             }
 
             // se for baú e não tem a quantidade de chaves o suficiente
@@ -277,11 +277,10 @@ int RecursaoGloboBT(TMapa* mapa, TMapa* resultado, int posX, int posY, int movim
             // se for baú e tem a quantidade de chaves o suficiente
             else if((chavesObtidas == mapa->qntChaves) && ehBau(mapa, xn, yn)){
                 // printf("Total de chaves: %d\n", chavesObtidas);
-                // mostraResultadoGrafico(mapa);
-                printf("\n");
                 // mostra todas as possibilidades de resultado
                 if(mostrarPossibilidades == 'S'){
                     printf("Possivel possibilidade:\n");
+                    mostraResultadoGrafico(mapa);
                     mostraVetorResultadoGlobo(mapa, 0, 0, 1);
                 }
 
@@ -314,24 +313,39 @@ int RecursaoGloboBT(TMapa* mapa, TMapa* resultado, int posX, int posY, int movim
     }
 }
 
+
 int mostraVetorResultadoGlobo(TMapa *resultado, int X, int Y, int passo){
+    printf("[0][0],");
+    recursaoVetorGlobo(resultado, X, Y, passo);
+    return 0;
+}
 
-    if (X < 0) X = (resultado->linha)-1;
-    if (X >= resultado->linha) X = 0;
-    if (Y < 0) Y = (resultado->coluna)-1;
-    if (Y >= resultado->coluna) Y = 0;
+int recursaoVetorGlobo(TMapa *resultado, int X, int Y, int passo){
+    int movimentoX[4] = {0, 1, 0, -1}, movimentoY[4] = {1, 0, -1, 0}, xn, yn;
 
-    if (ehBau(resultado, X, Y)){
-        printf("[%d][%d]\n", X, Y);
-        return 0;
+    for (int c = 0; c < 4; c++){
+        xn = X + movimentoX[c];
+        yn = Y + movimentoY[c];
+
+        // printf("X:%d Y:%d\n", xn, yn);
+        if (xn < 0) xn = (resultado->linha)-1;
+        if (xn >= resultado->linha) xn = 0;
+        if (yn < 0) yn = (resultado->coluna)-1;
+        if (yn >= resultado->coluna) yn = 0;
+
+        if (ehBau(resultado, xn, yn)){
+            printf("[%d][%d]\n", xn, yn);
+            return 0;
+        }
+        if (ehParede(resultado, xn, yn)){
+            //printf("ehParede\n");
+            continue;
+        } 
+
+
+        if (resultado->mapa[xn][yn].passo == passo+1) {
+            printf("[%d][%d],", xn, yn);
+            recursaoVetorGlobo(resultado, xn, yn, passo+1);
+        }
     }
-
-    printf("[%d][%d],", X,Y);
-
-
-    if (resultado->mapa[X+1][Y].passo == passo+1) mostraVetorResultado(resultado, X+1, Y, passo+1);
-    else if (resultado->mapa[X][Y+1].passo == passo+1) mostraVetorResultado(resultado, X, Y+1, passo+1);
-    else if (resultado->mapa[X-1][Y].passo == passo+1) mostraVetorResultado(resultado, X-1, Y, passo+1);
-    else if (resultado->mapa[X][Y-1].passo == passo+1) mostraVetorResultado(resultado, X, Y-1, passo+1);
-
 }
