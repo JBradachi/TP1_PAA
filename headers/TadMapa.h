50705e6d0ba0538c3642e90cd35b4616ce2cfa1a
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2023-10-23 13:25:23 -0300

    Removido comentarios

--- headers/TadMapa.h
+++ headers/TadMapa.h
@@ -29,13 +29,6 @@ typedef struct Celula {
     int passo; // passo do indiana bones, se 0 então ele não passou por lá
 }TCelula;
 
-
-// Como salvar a matriz? a matriz resposta faz parte do tad ou é uma matriz separada?
-// para o caso de mostrar todas as possibilidades de caminhos colocar flag na recursãoBT e dentro dela chamar o mostra matriz
-// é interesante criar um tad resposta pra ir guardando a matriz e se possúi resposta, porque vai acomulando respostas certas e queremos encontrar amelhor solução
-// a flag q ainda continua sendo útil para percorrer a matriz e ir voltando os passos
-// o fim de RecursaoBT sempre será 0 pois BT tentará fazer todos os caminhos possíveis
-
 typedef struct Mapa{
     TCelula** mapa;
     int linha;
